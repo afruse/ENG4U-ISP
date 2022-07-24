25477ddb7185a1a2dda36e28f07acd581895e14a
Affan <349980748@gapps.yrdsb.ca>
2022-07-24 13:55:53 -0400

    Added prop classes

---
+++ PC.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PC here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PC extends Actor
+{
+    /**
+     * Act - do whatever the PC wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
