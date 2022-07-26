d950e086adadd634bf0b5cb26cab01e8de8fb792
Affan <349980748@gapps.yrdsb.ca>
2022-07-25 20:01:55 -0400

    re-added props and remade them with invisible backgrounds

--- MyWorld.java
+++ MyWorld.java
@@ -8,11 +8,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    
+    public Bed bed;
+    public PC pc;
+    public Door door;
     public MyWorld()
     {    
         super(800, 600, 1);
+        bed = new Bed();
+        pc = new PC();
+        door = new Door();
         MainC main = new MainC();
         addObject(main, 400, 300);
+        addObject(bed, 76, 161);
+        addObject(pc, 450, 70);
+        addObject(door, 680, 9);
     }
 }
--- PC.java
+++ PC.java
@@ -12,6 +12,11 @@ public class PC extends Actor
      * Act - do whatever the PC wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public PC(){
+        GreenfootImage image = getImage();
+        image.scale(image.getWidth()/2, image.getHeight()/2);
+        setImage(image);
+    }
     public void act()
     {
         // Add your action code here.
