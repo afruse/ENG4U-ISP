b36c5641519829a5e4762e8742622a8aa79ab8fc
Affan <349980748@gapps.yrdsb.ca>
2022-07-26 11:09:33 -0400

    Edited Flashback world

--- Flashback.java
+++ Flashback.java
@@ -16,6 +16,12 @@ public class Flashback extends World
     public Flashback()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1); 
+        super(800, 600, 1);
+        setBackground("flash.jpg");
+        Outside.first.setValue("\"Long time ago you used to work for the");
+        Outside.second.setValue("person at the door. His name is Charles\"");
+        addObject(MyWorld.text, 400, 400);
+        addObject(Outside.first, 315, 520);
+        addObject(Outside.second, 323, 560);
     }
 }
